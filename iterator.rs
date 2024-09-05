ea479a14e14df5174799a3851a94d41e20c8e449
Diego Aguilar <d.aguilaro99@gmail.com>
2024-09-05 09:30:32 -0500

    iterator

--- iterator.rs
+++ iterator.rs
@@ -6,6 +6,7 @@
 // }
 //
 // Any type that implements Iterator must define the associated type 'Item' and the `next` method
+
 // The derive macro implements these traits for Trade:
 // Debug: Allows printing with {:?}
 // PartialEq: Allows comparison with ==
@@ -14,7 +15,7 @@
 struct Trade {
     id: u64,
     price: f64,
-    timestamp: u64,
+    block: u64,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -23,15 +24,11 @@ struct Intent {
     acceptable_trades: Vec<Trade>,
 }
 
+// Defines the data stucture to be traversed and the pointers to be used
 struct IntentIter {
     intents: Vec<Intent>,
-    intent_index: usize, // Unsigned integer native to the cpu pointer size
+    intent_index: usize, // Unsigned integer w pointer size naitive to cpu
     trade_index: usize,
-}
-
-impl Iterator for IntentIter {
-    type Item = Trade;
-    fn next(&mut self) -> Option<Self::Item> {}
 }
 
 impl IntentIter {
@@ -44,10 +41,58 @@ impl IntentIter {
     }
 }
 
+impl Iterator for IntentIter {
+    type Item = Trade;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.intent_index < self.intents.len() {
+            let intent = &self.intents[self.intent_index];
+            if self.trade_index < intent.acceptable_trades.len() {
+                let trade = intent.acceptable_trades[self.trade_index].clone();
+                self.trade_index += 1;
+                return Some(trade);
+            }
+            self.intent_index += 1;
+            self.trade_index = 0;
+        }
+        None
+    }
+}
+
+
 fn trades_above_price(intents: Vec<Intent>, min_price: f64) -> Vec<Trade> {
     IntentIter::new(intents)
         .filter(|t| t.price > min_price)
         .collect()
 }
 
-fn main() {}
+fn main() {
+    let intents = vec![
+        Intent {
+            user_id: 0,
+            acceptable_trades: vec![
+                Trade {id: 49, price: 100.01, block: 20681603},
+                Trade {id: 49, price: 98.02, block: 20681604},
+            ]
+        },
+        Intent {
+            user_id: 1,
+            acceptable_trades: vec![
+                Trade {id: 50, price: 100.05, block: 20681603},
+                Trade {id: 50, price: 99.8, block: 20681604},
+            ]
+        }
+    ];
+
+    let intent_iter = IntentIter::new(intents.clone());
+
+    for trade in intent_iter {
+        println!("Trade {:?}", trade);
+    }
+    
+    println!("\nTrades over 100");
+    let trades_over_100 = trades_above_price(intents.clone(), 100.00);
+    for trade in trades_over_100 {
+        println!("Trade {:?}", trade);
+    }
+}
